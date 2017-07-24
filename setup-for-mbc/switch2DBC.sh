387e686cddce699e7cd0d76c4f7532e854e997e6
Jonathan <839560084@qq.com>
2017-07-24 13:44:39 +0800

    modify to script to switch

--- setup-for-mbc/switch2DBC.sh
+++ setup-for-mbc/switch2DBC.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sed -i "s/MBC/DBC/g" `grep MBC -rl ../www`
-sed -i "s/魔币/多宝金豆/g" `grep 魔币 -rl ../www`
-sed -i "s/Mobicoin/dbcash/g" `grep Mobicoin -rl ../www`
+sed -i "s/DBC/DBC/g" `grep DBC -rl ../`
+sed -i "s/多宝金豆/多宝金豆/g" `grep 多宝金豆 -rl ../`
+sed -i "s/dbcash/dbcash/g" `grep dbcash -rl ../`
