eed7fecb794b62f17812e1939f3f3974a18499ec
Jonathan <839560084@qq.com>
2017-07-24 15:36:34 +0800

    make switch.sh even better

--- setup-for-mbc/switch2DBC.sh
+++ setup-for-mbc/switch2DBC.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sed -i "s/DBC/DBC/g" `grep DBC -rl ../`
-sed -i "s/多宝金豆/多宝金豆/g" `grep 多宝金豆 -rl ../`
-sed -i "s/dbcash/dbcash/g" `grep dbcash -rl ../`
+sed -i "s/MBC/DBC/g" `grep --exclude-dir={setup-for-mbc,../.*} MBC -rl ../`
+sed -i "s/魔币/多宝金豆/g" `grep --exclude-dir={setup-for-mbc,../.*} 魔币 -rl ../`
+sed -i "s/Mobicoin/Dbcoin/g" `grep --exclude-dir={setup-for-mbc,../.*} Mobicoin -rl ../`
