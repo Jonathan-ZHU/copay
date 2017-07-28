4fed557a3f7f3c2f32979d2e3521c0ae991ba722
TinyCalf <839560084@qq.com>
2017-07-28 17:18:12 +0800

    compiled on ios

--- DO_NOT_TOUCH/Unit2DBC.sh
+++ DO_NOT_TOUCH/Unit2DBC.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "正在修改单位至DBC，请勿轻易使用该环境开发，也不要提交产生的git修改!" && \
-sed  "" "s/MBC/DBC/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup* MBC -rl ./` && \
-sed  "" "s/魔币/多宝金豆/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup*  魔币 -l ./` && \
-sed  "" "s/Mobicoin/Dbcoin/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup* Mobicoin -rl ./` && \
+echo "正在修改单位至DBC，请勿轻易使用该环境开发，也不要提交产生的git修改!"
+sed "" "s/MBC/DBC/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup* MBC -rl ./`
+sed "" "s/魔币/多宝金豆/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup*  魔币 -rl ./`
+sed "" "s/Mobicoin/Dbcoin/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup* Mobicoin -rl ./`
 echo "修改单位至DBC成功!"
