6674c46e9285259bd2aeafef7a6f8ad0f30b5bc5
Jonathan <839560084@qq.com>
2017-08-05 14:43:45 +0800

    基本测试切换功能

--- DO_NOT_TOUCH/Mobicoin_url.sh
+++ DO_NOT_TOUCH/Mobicoin_url.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+bwsrpl="{{BWSURL}}"
+bws="https\:\/\/bws\.tiny-calf\.com\/bws\/api"
+insightrpl="{{INSIGHTURL}}"
+insight="https\:\/\/blockchain\.browser\.tiny-calf\.com\/insight"
 echo "正在修改URL至Mobicoin，请勿轻易使用该环境开发，也不要提交产生的git修改!"
-sed "" "s/{{BWSURL}}/https:\/\/bws.tiny-calf.com\/bws\/api/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup* {{BWSURL}} -rl ./`
-sed "" "s/{{INSIGHTURL}}/https:\/\/blockchain.browser.tiny-calf.com\/insight/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup*  {{INSIGHTURL}} -rl ./`
+sed -i "s/${bwsrpl}/${bws}/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup* ${bwsrpl} -rl ./`
+sed -i "s/${insightrpl}/${insight}/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup*  ${insightrpl} -rl ./`
 echo "修改URL至Mobicoin成功"
--- DO_NOT_TOUCH/Tcash_url.sh
+++ DO_NOT_TOUCH/Tcash_url.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-echo "正在修改URL至Tcash，请勿轻易使用该环境开发，也不要提交产生的git修改!"
-sed "" "s/{{BWSURL}}/https:\/\/tcash.bws.tiny-calf.com\/bws\/api/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup* {{BWSURL}} -rl ./`
-sed "" "s/{{INSIGHTURL}}/https:\/\/tcash.insight.tiny-calf.com\/insight/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup*  {{INSIGHTURL}} -rl ./`
-echo "修改URL至Tcash成功"
+bwsrpl="{{BWSURL}}"
+bws="https\:\/\/tcash\.bws\.tiny-calf\.com\/bws\/api"
+insightrpl="{{INSIGHTURL}}"
+insight="https\:\/\/tcash\.insight\.tiny-calf\.com\/insight"
+echo "正在修改URL至Mobicoin，请勿轻易使用该环境开发，也不要提交产生的git修改!"
+sed -i "s/${bwsrpl}/${bws}/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup* ${bwsrpl} -rl ./`
+sed -i "s/${insightrpl}/${insight}/g" `grep --exclude-dir=app-template --exclude-dir=DO_NOT_TOUCH --exclude-dir=.git --exclude=setup*  ${insightrpl} -rl ./`
+echo "修改URL至Mobicoin成功"
