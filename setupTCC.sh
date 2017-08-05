70564524362e23f889292c0a304eab2be3db3997
Jonathan <839560084@qq.com>
2017-08-05 16:32:12 +0800

    add network for tcash

--- setupTCC.sh
+++ setupTCC.sh
@@ -2,7 +2,7 @@
 echo "正在构建DBC钱包"
 ./DO_NOT_TOUCH/Tcash_url.sh && \
 npm run apply:tcash && \
-./DO_NOT_TOUCH/setupNetwork.sh && \
+./DO_NOT_TOUCH/setupNetwork-tcash.sh && \
 ./DO_NOT_TOUCH/macbug1.sh && \
 ./DO_NOT_TOUCH/Unit2TCC.sh && \
 echo "完成DBC钱包构建！"
