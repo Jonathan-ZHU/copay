79d758532ffe3f83928b810501e9639a88d82f9d
ztryxh <18410136179@163.com>
2017-08-23 10:40:32 +0800

    修改服务器地址

--- app-template/tcash/app.js
+++ app-template/tcash/app.js
@@ -22,14 +22,14 @@ var modules = [
 
 //change urls here
 
-var insight_testUrl="https://tcash.insight.tiny-calf.com/insight";
-var insight_Normalurl="https://insight.jifapi.cn/insight";
+var insight_testUrl="https://ttcash.insight.tiny-calf.com/insight";
+var insight_Normalurl="https://tcash.insight.tiny-calf.com/insight";
 var insight_url = insight_Normalurl;
-var bwe_testUrl="https://tcash.bws.tiny-calf.com/bws/api"
-var bws_NormalUrl="https://bws.jifapi.cn/bws/api";
+var bwe_testUrl="https://ttcash.insight.tiny-calf.com/bws/api"
+var bws_NormalUrl="https://tcash.bws.tiny-calf.com/bws/api";
 var bws_url =bws_NormalUrl ;
 var ico_Testserver="https://120.92.35.170:8080";
-var ico_NormalUrl="https://120.92.91.36:8080";
+var ico_NormalUrl="http://tcash.ico.tiny-calf.com/";
 var ico_server=ico_NormalUrl;
 //change coin unit here
 var coin_unit = 'TCC';
