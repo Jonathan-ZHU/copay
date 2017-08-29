d8af7aee765d606f34d2cb4911921640ff37632a
ztryxh <18410136179@163.com>
2017-08-29 16:14:14 +0800

    修改币种

--- app-template/EBOCoin/po/change.sh
+++ app-template/EBOCoin/po/change.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-en="Tcash"
-zh="TCC"
-zhHK="TCC"
-sed -i "s/魔幣/${zhHK}/g" `grep 魔幣 -rl ./it.po`
-sed -i "s/魔币/${zh}/g" `grep   魔币 -rl ./zh.po`
-sed -i "s/MMM/${en}/g" `grep MMM -rl ./en.po`
+en="EBOCoin"
+zh="EBOCoin"
+zhHK="EBOCoin"
+sed -i "s/TCC/${zhHK}/g" `grep TCC -rl ./it.po`
+sed -i "s/TCC/${zh}/g" `grep   TCC -rl ./zh.po`
+sed -i "s/Tcash/${en}/g" `grep Tcash -rl ./en.po`
 echo "修改成功!"
